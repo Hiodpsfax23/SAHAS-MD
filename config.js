7807062fccf031786868e222059d393e5c0ae1c8
Hiodpsfax23 <diviner419@gmail.com>
2024-11-20 09:06:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "ZW8yDDob#RW960UBIUaI55fGXd1u8Fu_vu_jVRX7uYf3faoTMzhk",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~ivIj2KrR#Z7suP06ohKnEIq10CMeDJtyatQc_GiH7z9QUN0f1hHY",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
